3c5d5d87c88629cdb60fa7310a0022130bcd5510
glokayakuzdra <marinaseniorhtmldeveloper@gmail.com>
2026-01-09 21:46:56 +0400

    Fix render port

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// Раздача статических файлов из корневой директории
-app.use(express.static(path.join(__dirname)));
+// Раздача статических файлов из текущей папки
+app.use(express.static(__dirname));
 
 // Запуск сервера
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
