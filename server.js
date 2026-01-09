d550642ca230ea73512b3332cd7164d228eed85e
glokayakuzdra <marinaseniorhtmldeveloper@gmail.com>
2026-01-09 21:38:37 +0400

    Add Node.js server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Раздача статических файлов из корневой директории
+app.use(express.static(path.join(__dirname)));
+
+// Запуск сервера
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
